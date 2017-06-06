f5ac03998204339d807f0668d2af0639d8130fa4
John Vonelli <stjpv03@moravian.edu>
2017-06-06 11:50:48 -0400

    Separated testing and function file, removed Jenkinsfile.

---
+++ jenkins_func.py
@@ -0,0 +1,5 @@
+def func(f):
+    if(f == 'Hello'):
+        return "World"
+    else:
+        return 'Hello'
--- jenkinstests.py
+++ jenkinstests.py
@@ -1,9 +1,2 @@
-def func(f):
-    if(f == 'Hello'):
-        return "World"
-    else:
-        return 'Hello'
-
-
 def test_answer():
     assert func('Hello') == 'World'
