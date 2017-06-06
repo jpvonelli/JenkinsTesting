bea5921a8dab95659502b83052d6482c60506a3a
Charles Peeke <stcdp03@moravian.edu>
2017-06-06 15:32:11 -0400

    Update jenkins_func.py

--- jenkins_func.py
+++ jenkins_func.py
@@ -2,7 +2,9 @@
   This is a function that simulates Hello World
 """
 def func(word):
-    # If the word given is Hello, it will give back World
+    """
+    If the word given is Hello, it will give back World
+    """
     if word == 'Hello':
-        return "World"
+        return 'World'
     return 'Hello'
